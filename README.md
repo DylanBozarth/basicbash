f6885b9132e9b86271b92e9ddf211b1995e63a49
imstupidpleasehelp <dylanabozarth@gmail.com>
2021-12-10 20:23:58 -0800

    basic commands

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # basicbashscript
 # basicbash
+https://www.youtube.com/watch?v=v-F3YLd6oMw&t=123s
+9:46
--- myscript.sh
+++ myscript.sh
@@ -1,2 +1,19 @@
 #! /usr/bin/bash
-echo aaa
+
+# console.log()
+#echo aaa
+
+# Variables UPPERCASE 
+NAME="Dylan"
+echo "Your name is $NAME"
+
+#user input
+#read -p "your name " THEIRNAME
+#echo "what's up, $THEIRNAME?"
+
+#! IF STATEMENT 
+if [ $NAME == "Dylan" ]
+then 
+echo "Nice"
+# end the if statement with fi, for some reason
+fi 
